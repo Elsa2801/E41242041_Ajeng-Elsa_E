b0474583888c93b88a2caec644b7f8388968d2dc
Elsa2801 <ajengelsa6592@gmail.com>
2025-02-08 17:37:30 +0700

    Update Helloooo.java

--- Helloooo.java
+++ Helloooo.java
@@ -14,7 +14,7 @@ public class Helloooo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(" Hi, Saya Ajeng");
+        System.out.println(" Hi, Saya Ajeng yow");
     }
     
 }
