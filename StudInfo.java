1c894b7402d9de22af0e5066b152880f3d8f887e
Elsa2801 <ajengelsa6592@gmail.com>
2025-02-13 15:55:54 +0700

    Add files via upload

---
+++ StudInfo.java
@@ -0,0 +1,16 @@
+
+package studinfo;
+
+public class StudInfo {
+    public static void main(String[] args) {
+        String fName = "Lisa";
+        String lName = "Palombo";
+        String stuId = "123456789";
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name: " + fName + " " + lName);
+        System.out.println("Student ID: " + stuId);
+        System.out.println("Student Status: " + stuStatus);
+    }
+    
+}
