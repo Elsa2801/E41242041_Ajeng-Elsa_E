d54e71657d2de85feaf86c57dc092f35f5cf2c4a
Elsa2801 <ajengelsa6592@gmail.com>
2025-02-13 16:00:53 +0700

    Add files via upload

---
+++ PersonJava.java
@@ -0,0 +1,26 @@
+
+package person.java;
+
+public class PersonJava {
+    String Name;
+    int Age;
+    
+    public PersonJava(String Name, int Age) {
+        this.Name = Name;
+        this.Age = Age;
+    }
+    
+    public String getName() {
+        return Name;
+    }
+    
+    public int getAge() {
+        return Age;
+    }
+    
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+   
+    
+}
