aa404dbd244e7451a91ffb3b0e51f90cb3ffd37d
Elsa2801 <ajengelsa6592@gmail.com>
2025-02-16 20:29:18 +0700

    Add files via upload

---
+++ statement_do_while.java
@@ -0,0 +1,11 @@
+package statement_while;
+public class statement_do_while {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while (i <= 10);
+    }
+}
